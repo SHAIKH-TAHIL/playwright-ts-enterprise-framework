4b6898d847d2ce986c43d7894a365b4039609d64
shaikh, tahil <tahil.shaikh+mcgraw@mheducation.com>
2026-02-22 16:47:01 +0530

    - Added TSDOCs

--- src/pages/inventoryPage.ts
+++ src/pages/inventoryPage.ts
@@ -14,21 +14,31 @@ export class InventoryPage {
     private readonly PRODUCT_TITLE_TEXT = "";
     private readonly PRODUCT_SORT_DROPDOWN = "";
 
-    // Getters for all the Locators
+    /**
+     * @return the locator for the hamburger menu icon
+     */
     get hamburgerMenuIcon () {
         return this.page.locator(this.HAMBURGER_MENU_ICON);
     }
 
+    /**
+     * @return the locator for the product container element
+     */
     get productContainer () {
         return this.page.locator(this.PRODUCT_CONTAINER);
     }
 
+    /**
+     * @return the locator for the product title text element
+     */
     get productTitleText () {
         return this.page.locator(this.PRODUCT_TITLE_TEXT);
     }
 
+    /**
+     * @return the locator for the product sort dropdown element
+     */
     get productSortDropdown () {
         return this.page.locator(this.PRODUCT_SORT_DROPDOWN);
     }
-
 }
--- src/utils/commonFunctions.ts
+++ src/utils/commonFunctions.ts
@@ -7,10 +7,20 @@ export class CommonFunctions {
         this.page = page;
     }
 
+    /**
+     * @description Verifies that the current page URL matches the expected URL.
+     * @param expectedUrl The expected URL to compare against the current page URL.
+     */
     async verifyPageUrl (expectedUrl: string) {
         await expect(this.page).toHaveURL(expectedUrl);
     }
 
+    /**
+     * @description Verifies that the text of a given element matches the expected text.
+     * @param locator The Playwright Locator for the element to verify.
+     * @param expectedText The expected text to compare against the element's text.
+     * @param partialMatch If true, checks if the element's text contains the expected text; otherwise, checks for an exact match.
+     */
     async verifyTextOfAnElement (locator: Locator, expectedText: string, partialMatch: boolean = false) {
         if (partialMatch) {
             await expect(locator).toContainText(expectedText);
